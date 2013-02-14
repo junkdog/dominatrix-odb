ca7bb85359f94028f8d800a5003adccf124c2337
Adrian Papari <blackrax@gmail.com>
2013-02-14 09:15:21 +0100

    Removed notification hud from inspector hud.

--- dominatrix-gdx-odb/src/net/onedaybeard/experimental/ui/EntityInspectorHud.java
+++ dominatrix-gdx-odb/src/net/onedaybeard/experimental/ui/EntityInspectorHud.java
@@ -1,7 +1,6 @@
 package net.onedaybeard.experimental.ui;
 
 import lombok.Setter;
-import net.onedaybeard.annotation.Sloppy;
 import net.onedaybeard.artemis.JsonEntityMarshaller;
 import net.onedaybeard.experimental.artemis.CommandUtils;
 import net.onedaybeard.experimental.artemis.CommandUtils.ObjectNode;
@@ -9,15 +8,12 @@ import net.onedaybeard.util.Tree;
 
 import com.artemis.Entity;
 import com.artemis.World;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.JsonWriter.OutputType;
 
-@Sloppy("")
 public final class EntityInspectorHud
 {
 	private Skin skin;
@@ -25,14 +21,10 @@ public final class EntityInspectorHud
 	private Table table;
 	private Label hovered;
 	
-	private Table statusTable;
-	private Label status;
-	
 	private Entity lastEntity;
 	
 	private enum View {ENTITY, JSON};
 	private View view;
-
 	
 	@Setter private JsonKeyResolver jsonKeyResolver;
 	
@@ -44,20 +36,6 @@ public final class EntityInspectorHud
 		
 		initialize(world);
 		ui.addActor(table);
-		
-		status = new Label("", skin);
-		
-		statusTable = new Table(skin);
-		statusTable.getColor().a = 0;
-		
-		statusTable.setPosition(20, Gdx.graphics.getHeight() - statusTable.getMaxHeight());
-		statusTable.defaults().pad(5).fillX().expandX();
-		statusTable.setBackground(BackgroundTexture.getDrawable());
-		statusTable.row().expandX().fillX();
-		statusTable.add(status);
-		statusTable.pack();
-		
-		ui.addActor(statusTable);
 		
 		table.setVisible(false);
 		view = View.ENTITY;
@@ -78,17 +56,6 @@ public final class EntityInspectorHud
 			hovered.setText("");
 		
 		table.pack();
-	}
-	
-	public void setStatusText(String text)
-	{
-		status.setText(text);
-		statusTable.pack();
-		statusTable.setPosition((Gdx.graphics.getWidth() - statusTable.getWidth()) / 2,
-			Gdx.graphics.getHeight() - statusTable.getHeight());
-		
-		statusTable.clearActions();
-		statusTable.addAction(Actions.sequence(Actions.fadeIn(0.5f), Actions.delay(3.5f), Actions.fadeOut(0.5f)));
 	}
 
 	protected void initialize(final World world)
